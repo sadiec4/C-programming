267716da62deac03f2c0f565b52b083248914d32
sadiec4 <scampsey0117@floridapoly.edu>
2025-01-16 16:26:42 +0000

    Implemented first program

---
+++ 01_firstprogram.c/01_firstprogram.c
@@ -0,0 +1,18 @@
+
+// comment
+
+/*
+multiline comment 
+*/ 
+
+
+#include <stdio.h>
+
+//create a main function
+int main () {
+    printf("welcome\nto\nCOP2271\n");
+    printf("I\nwant\nto\ncode!\n");
+
+    return 0;
+}
+
